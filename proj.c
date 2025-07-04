7392c9e8d43a117acdad0f61256c5142ed92de24
edusts-SI <edusts82@gmail.com>
2025-07-04 11:24:42 -0300

    Update proj.c

--- proj.c
+++ proj.c
@@ -1,5 +1,19 @@
-#include <stdio.h>
+#include <stdio.h> // nossa biblioteca padão
+#include <stdlib> // biblioteca para utilização do system (cls) para limpar o a tela para melhor visualização no terminal.
+#include <string.h> // Utilizaremos para manipular strings.
+#include <ctype.h> // manipulação de caracteries.
+#include <windows.h> // para melhor controle do console.
+#include <conio.h> // manipulação de cores para melhor visualização.
 
-int main (){
-    
-}
+//Constantes que serão utilizadas ao longo do codico
+
+#define TAMANHO 8 // tamanho da matriz (tabuleiro)
+//peças
+#define VAZIO ' '
+#define BRANCA 'b'
+#define PRETA 'p'
+#define dama_bra 'B'
+#define dama_prt 'P'
+
+#define jogador_bra 1
+#define jogador_prt 2
